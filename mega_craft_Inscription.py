1d737be4e2084783a9df0bc9207d186b6c553f25
megastruktur <astromortis@gmail.com>
2021-05-12 11:01:15 +0300

    Update mega_craft_Inscription.py
    Inscription tweaks

--- mega_craft_Inscription.py
+++ mega_craft_Inscription.py
@@ -1,26 +1,38 @@
 #Misc.Inspect()
 
+from Scripts.utilities.colors import colors
+
 inscriptionToolType = 0x0FBF
 inscriptionGump = 2066278152
+gump_CraftLast = 4
+gump_LastTen = 28
+manaStartRegen = 45
 
 def TrainInscription():
-    
 
     while Player.GetSkillValue("Inscription") < 100:
 
+        if Player.Hits * 100 / Player.HitsMax < 30:
+            Player.UseSkill( 'Spirit Speak' )
+            Misc.Pause(2000)
+    
         inscriptionTool = getByItemID(inscriptionToolType, Player.Backpack.Serial)
         Items.UseItem(inscriptionTool)
+        Misc.Pause(1000)
+        
+        Gumps.WaitForGump(inscriptionGump, 10000)
+        Gumps.SendAction(inscriptionGump, gump_LastTen)
         Misc.Pause(500)
         Gumps.WaitForGump(inscriptionGump, 10000)
-        Gumps.SendAction(inscriptionGump, 4)
+        Gumps.SendAction(inscriptionGump, gump_CraftLast)
         
-        Misc.Pause(2000)
+        Misc.Pause(2500)
         
 
-        if Player.Mana < 35:
-            Misc.SendMessage("Waiting for mana to regenerate...")
+        if Player.Mana < manaStartRegen:
+            Misc.SendMessage("Waiting for mana to regenerate...", colors['yellow'])
             Player.UseSkill("Meditation")
-            while Player.Mana < 130:
+            while Player.Mana * 100/Player.ManaMax < 80:
                 Misc.Pause(1000)
         else:
             Gumps.WaitForGump(inscriptionGump, 10000)
